1f74a1decda46473abed0d60f0d0c89614f6a1c0
gianmarcotaf02 <gianmarco.taf02@gmail.com>
2026-08-27 11:00:59 +0200

    checkpoint: auto-save at turn 2 [2026-08-27T09-00-59]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.google.services)
@@ -39,9 +40,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
     }
 
     applicationVariants.all {
