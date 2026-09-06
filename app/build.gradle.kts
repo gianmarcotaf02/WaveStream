4cf1fd9dc3f4b71536d8a39eed17c4b475dbb11f
gianmarcotaf02 <gianmarco.taf02@gmail.com>
2026-09-06 17:52:09 +0200

    checkpoint: auto-save at turn 0 [2026-09-06T15-52-08]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "it.wavestream.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.0.12"
+        versionCode = 14
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
